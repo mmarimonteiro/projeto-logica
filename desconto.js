b089be1aab6b0acdb46613fb318d72713c8c5f92
Mariane Monteiro <marimonteironasciment@gmail.com>
2022-04-08 18:37:39 -0300

    projeto-logica

---
+++ desconto.js
@@ -0,0 +1,10 @@
+
+const valorProduto = 10
+const porcentagemDesconto = 20
+
+const valorDesconto = valorProduto*porcentagemDesconto/100 
+
+const valorfinal = valorProduto - valorDesconto
+
+console.log("o preço a pagar é: " + valorFinal)
+console.log("o valor do desconto é: " + valorDesconto)
