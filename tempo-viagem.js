16e052c0d0ae2f358142b8de35168f6b5b96605c
Mariane Monteiro <marimonteironasciment@gmail.com>
2022-04-08 19:03:15 -0300

    exercício tempo viagem

---
+++ tempo-viagem.js
@@ -0,0 +1,7 @@
+let distanciaPercorrer = 50 //considerando em km
+
+let velocidadeMedia = 30 //considerando km/h
+
+let tempoViagem = distanciaPercorrer/velocidadeMedia
+
+console.log("o tempo da viagem é: " + tempoViagem + " hora(s)")
